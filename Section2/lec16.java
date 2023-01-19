4eb1900341b1fa990f08a259c37a2e1ec5833319
Aamir041 <aamirsaudagar34@gmail.com>
2023-01-19 23:40:40 +0530

    lec16 added

---
+++ Section2/lec16.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+
+public class lec16 {
+	public static void main(String[] args) {
+		int postiveNum = 4;
+		int negativeNum = -4;
+		// getting x's value in binary form
+		System.out.println(Integer.toBinaryString(postiveNum));
+		System.out.println(Integer.toBinaryString(negativeNum));
+	}
+}
