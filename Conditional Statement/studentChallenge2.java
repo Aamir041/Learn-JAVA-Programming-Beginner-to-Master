0062136d7d28c32fdebb4cb04f771dee4b543601
Aamir041 <aamirsaudagar34@gmail.com>
2023-01-25 19:13:52 +0530

    added studentChallenge2.java

---
+++ Conditional Statement/studentChallenge2.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class studentChallenge2{
+	public static void main(String[] args){
+
+		Scanner in = new Scanner(System.in);
+		System.out.printf("Enter number: ");
+		String num = in.next();
+
+		if(num.matches("[01]*")){
+			System.out.printf("Number is binary, Radix is 2.\n");
+		}
+
+		else if(num.matches("[0-7]*")){
+			System.out.printf("Number is octal, Radix is 8.\n");
+		}
+
+		else if(num.matches("[0-9A-F]*")){
+			System.out.printf("Number is hexadecimal, Radix is 16.\n");
+		}
+		else if(num.matches("[0-9]*")){
+			System.out.printf("Number is decimal, Radix is 10.\n");
+		}
+
+	}
+}
