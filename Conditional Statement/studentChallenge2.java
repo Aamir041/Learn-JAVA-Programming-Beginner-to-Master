d5b51b156c1e09ed1b717c82a4dd2d21dbafed73
Aamir041 <aamirsaudagar34@gmail.com>
2023-01-25 19:18:20 +0530

    added studentChallenge2.java

--- Conditional Statement/studentChallenge2.java
+++ Conditional Statement/studentChallenge2.java
@@ -3,24 +3,30 @@ import java.util.*;
 public class studentChallenge2{
 	public static void main(String[] args){
 
+// Find radix of a given String
+
 		Scanner in = new Scanner(System.in);
 		System.out.printf("Enter number: ");
 		String num = in.next();
 
-		if(num.matches("[01]*")){
+		if(num.matches("[01]+")){
 			System.out.printf("Number is binary, Radix is 2.\n");
 		}
 
-		else if(num.matches("[0-7]*")){
+		else if(num.matches("[0-7]+")){
 			System.out.printf("Number is octal, Radix is 8.\n");
 		}
 
-		else if(num.matches("[0-9A-F]*")){
+		else if(num.matches("[0-9A-F]+")){
 			System.out.printf("Number is hexadecimal, Radix is 16.\n");
 		}
-		else if(num.matches("[0-9]*")){
+		else if(num.matches("[0-9]+")){
 			System.out.printf("Number is decimal, Radix is 10.\n");
 		}
+
+
+// Find if a given year is leap year or not
+
 
 	}
 }
