34a75f946ceed86fe46f9eb854c0355d1ebe3dc0
Aamir041 <aamirsaudagar34@gmail.com>
2023-01-28 00:07:07 +0530

    added pattern4

---
+++ Loops/pattern4.java
@@ -0,0 +1,13 @@
+public class pattern4{
+	public static void main(String[] args){
+		int counter = 1;
+		for(int i = 1; i<=5; i++){
+		    for(int j = 1; j<=5; j++){
+		    	System.out.printf("%2d ",counter);
+		    	counter++;
+		    }
+		    System.out.println();
+		
+		}
+	}
+}
