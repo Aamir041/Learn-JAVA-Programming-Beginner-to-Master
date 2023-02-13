e1adb935b2841d911eb0cebce38fd330d3149950
Aamir041 <aamirsaudagar34@gmail.com>
2023-02-13 10:27:23 +0530

    added default Method revised code

--- OOPs/Interfaces/defaultMethod.java
+++ OOPs/Interfaces/defaultMethod.java
@@ -1,29 +1,43 @@
-interface test {
-    static final int X  = 100;
-    void meth1();
-    default void meth2(){
-        System.out.println("This is meth2");
+interface sayHello{
+    void greet();
+    void greet(String name);
+    default void sayHi(){
+        System.out.println("Hallo");
     }
-    static void meth3(){
-        System.out.println("This is meth3");
-    }
+
+    static void sayHola(){
+        System.out.println("\nHola Amigos");   
+    } 
 }
 
-class test1 implements test{
+class englishGreet implements sayHello{
+    
+    @Override   
+    public void greet(){
+        System.out.println("Hello");
+    }
+
     @Override
-    public void meth1(){
-        System.out.println("This is meth1");
+    public void greet(String name){
+        System.out.println("Hello "+name);
     }
 }
 
-public class defaultMethod {
+public class HelloWorld{
     public static void main(String[] args) {
-        test.meth3(); // calling static method
-        System.out.println(test.X); // this is identifier
-        // test.meth2(); can not use like this
-        test1 t1 = new test1();
-        t1.meth2();
-        t1.meth1();
-        // t1.meth3(); -> implemented class can not call static method from interface
+        sayHello h1 = new englishGreet();
+        h1.sayHi();
+        h1.greet();
+        h1.greet("Aamir");
+        // h1.sayHola(); // can not call static method using a object reference
+        sayHello.sayHola();
+
+        System.out.println();
+
+        englishGreet e1 = new englishGreet();
+        e1.sayHi();
+        e1.greet();
+        e1.greet("Aamir Saudagar");
+        // e1.sayHola(); // can not call static method using a object reference         
     }
 }
