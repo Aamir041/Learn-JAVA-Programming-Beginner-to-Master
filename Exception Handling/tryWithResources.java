7ab98c10e92051240e26ba6a3e7ff623703f6a22
Aamir041 <aamirsaudagar34@gmail.com>
2023-02-17 13:47:03 +0530

    added

---
+++ Exception Handling/tryWithResources.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class tryWithResources{
+
+	static void openFile(String fileName){
+		try(FileReader f = ){
+
+		}
+	}
+
+	public static void main(String[] args){
+
+
+	}
+}
